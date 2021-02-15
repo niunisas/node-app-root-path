3f28ceed7c70a39084a7074de6b36d0e1744345f
Arnas Kazlauskas <55489629+niunisas@users.noreply.github.com>
2021-02-15 12:15:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-const process = require('process'); 
 
-var lib = require('./lib/app-root-path.js');
-module.exports = lib(process.cwd());
+//var lib = require('./lib/app-root-path.js');
+//module.exports = lib(__dirname);
